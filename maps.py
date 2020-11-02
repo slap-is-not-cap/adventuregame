1f532c6813afe031e948fb684253c105605c415a
MMaxM <kaibergman22@gmail.com>
2020-11-02 05:22:35 +0000

    e

--- maps.py
+++ maps.py
@@ -30,19 +30,66 @@ def outputArray(coords):
 					pass
 				else:
 					ch = coords[i][j]
-					print(ch + "   ", end = '')
+					print(str(ch) + "   ", end = '')
 				e = False
 		print("\n")	
 
 
-def randomizeMap(coords,x,y):
+def randomizeMap(coords:list,x:int,y:int):
+	allow = True
+	eCycle = False
+	sCycle = False
+	currentX = 0
+	currentY = 0
+	start = random.randint(x,y)
+	coords[0][0] = start
 	x_size = len(coords)
 	y_size = len(coords[0])
 	for j in range (0,y_size):
 		for i in range(0,x_size):
-			ch = coords[i][j]
-			biomegen = random.randint(x,y)
-			coords[i][j] = str(biomegen)
+			if allow == False:
+				pass
+			else:
+				percent = random.randint(1,10)
+			if eCycle == False:
+				if percent == 6:
+					if currentX+1 == 9 or currentX ==9:
+						pass
+					else:
+						coords[currentY][currentX+1] = start
+						eCycle = True
+				else:
+					if currentX+1 >= 9 or currentX >= 9:
+						pass
+					else:
+						print (currentY)
+						allow = False
+						loc = random.randint(x,y)
+						coords[currentY+1][currentX] = loc
+
+			if sCycle == False:
+				if percent == 6:
+					if currentY == 9:
+						pass
+					else:
+						coords[currentY][currentX-1] = start
+				else:
+					if currentX+1 == 9 or currentX ==9:
+						pass
+					else:
+						if currentY+1 or currentY == 9:
+							pass
+						else:
+							allow = False
+							loc = random.randint(x,y)
+							print('loc'+str(loc))
+							coords[currentY][currentX+1] = loc
+			#currentX +=1
+			#currentY +=1
+
+			#ch = coords[i][j]
+			#biomegen = random.randint(x,y)
+			#coords[i][j] = str(biomegen)
 	
 			
 			
